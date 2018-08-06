90de8e7b25f7f097939a002660c8c0588dba67ed
Ellen Schmidt <38501884+ellschmidt@users.noreply.github.com>
2018-08-06 14:31:47 +0200

    Upload the rest

---
+++ trial_info/main_trials.js
@@ -0,0 +1,37 @@
+var main_trials = [
+	{question: "After working at a horse farm for several years, Paul got a job at the \
+			   as the main trainer and rides many different horses each day. This is the \
+			   job he has always wanted. Almost every single day Paul feels good and generally \
+			   experiences a lot of pleasant emotions. In fact, it is very rare that he \
+			   would ever feel negative emotions like sadness or loneliness. When Paul \
+			   thinks about his life, he always comes to the same conclusion: he feels highly \
+			   satisfied with the way he lives.", 
+	 questionadd: "The reason Paul feels this way is that he tries to support the horses by \
+					riding them with a special saddle.", 
+	 questionaddto: "Ten years after Paul died, a new discovery was made which suggested \
+					that the saddle had made some of the horses stronger."},
+	{question: "What is the weather like?", 
+	 questionadd: "shiny", 
+	 questionaddto: "rainbow"},
+    {question: "How are you today?", 
+     questionadd: "fine", 
+     questionaddto: "great"},
+    {question: "What is the weather like?", 
+     questionadd: "shiny", 
+     questionaddto: "rainbow"},
+    {question: "How are you today?", 
+     questionadd: "fine", 
+     questionaddto: "great"},
+    {question: "What is the weather like?", 
+     questionadd: "shiny", 
+     questionaddto: "rainbow"},
+    {question: "How are you today?", 
+     questionadd: "fine", 
+     questionaddto: "great"},
+    {question: "What is the weather like?", 
+     questionadd: "shiny", 
+     questionaddto: "rainbow"},
+    {question: "What is the weather like?", 
+     questionadd: "shiny", 
+     questionaddto: "ham"}
+];
